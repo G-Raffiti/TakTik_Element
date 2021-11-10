4818d0ae441bc3d27c0f50cb31a983099a626857
G-Raffiti <69368335+G-Raffiti@users.noreply.github.com>
2021-11-10 07:27:00 +0100

    ui fini pour la forge

--- Assets/Scripts/UserInterface/PersonalInventory.cs
+++ Assets/Scripts/UserInterface/PersonalInventory.cs
@@ -46,43 +46,28 @@ namespace UserInterface
             return ColouredName();
         }
 
+        
         public override string GetInfoLeft()
         {
             string str = "";
             str += $"<sprite name=AP> <color={colorSet.HexColor(EAffix.AP)}>{(int)total.AP}</color>    ";
-            str += $"<sprite name=MP> <color={colorSet.HexColor(EAffix.MP)}>{(int)total.MP}</color>\n";
-            str += $"<sprite name=HP> <color={colorSet.HexColor(EAffix.HP)}>{BattleStats.HP} </color>/ {total.HP}\n";
-            str += $"<sprite name=Shield> <color={colorSet.HexColor(EAffix.Shield)}>{BattleStats.Shield} </color>/ {total.Shield}\n"; 
-            str += $"<sprite name=Dodge> <color={colorSet.HexColor(EAffix.Dodge)}>{(int) BattleStats.Dodge} </color>/ {(int) total.Dodge}\n"; 
-            str += $"<sprite name=Speed> <color={colorSet.HexColor(EAffix.Speed)}>{BattleStats.Speed} </color> \n";
-            str += $"<sprite name=TP> <color={colorSet.HexColor(EColor.TurnPoint)}>{BattleStats.TurnPoint} </color> \n";
+            str += $"<sprite name=MP> <color={colorSet.HexColor(EAffix.MP)}>{(int)total.MP}</color> \n";
+            str += $"<sprite name=HP> <color={colorSet.HexColor(EAffix.HP)}>{BattleStats.HP} </color>/ {total.HP}    ";
+            str += $"<sprite name=Shield> <color={colorSet.HexColor(EAffix.Shield)}>{BattleStats.Shield}</color> \n";
+            str += $"<sprite name=Fire> <color={colorSet.HexColor(EAffix.Fire)}>{BattleStats.GetPower(EElement.Fire)}</color>  <sprite name=Water> <color={colorSet.HexColor(EAffix.Water)}>{BattleStats.GetPower(EElement.Water)}</color>  <sprite name=Nature> <color={colorSet.HexColor(EAffix.Nature)}>{BattleStats.GetPower(EElement.Nature)}</color>";
 
             return str;
         }
 
         public override string GetInfoRight()
         {
+            Unit u = null;
             string str = "";
-            str += $"Basic Power: {BattleStats.Power.Basic} \n";
-            str += $"Spell Power : <sprite name=Fire><color={colorSet.HexColor(EAffix.Fire)}>{BattleStats.Power.MagicPercent(EElement.Fire)}</color>  <sprite name=Water><color={colorSet.HexColor(EAffix.Water)}>{BattleStats.Power.MagicPercent(EElement.Water)}</color>  <sprite name=Nature><color={colorSet.HexColor(EAffix.Nature)}>{BattleStats.Power.MagicPercent(EElement.Nature)}</color> (%) \n";
-            str += $"Skill Power :  <sprite name=Fire><color={colorSet.HexColor(EAffix.Fire)}>{BattleStats.Power.PhysicPercent(EElement.Fire)}</color>  <sprite name=Water><color={colorSet.HexColor(EAffix.Water)}>{BattleStats.Power.PhysicPercent(EElement.Water)}</color>  <sprite name=Nature><color={colorSet.HexColor(EAffix.Nature)}>{BattleStats.Power.PhysicPercent(EElement.Nature)}</color> (%) \n";
-            str += $"Focus Power :  <sprite name=Fire><color={colorSet.HexColor(EAffix.Fire)}>{BattleStats.GetFocus(EElement.Fire)}</color>  <sprite name=Water><color={colorSet.HexColor(EAffix.Water)}>{BattleStats.GetFocus(EElement.Water)}</color>  <sprite name=Nature><color={colorSet.HexColor(EAffix.Nature)}>{BattleStats.GetFocus(EElement.Nature)}</color> \n";
-            str += $"Damage Taken :  <sprite name=Fire><color={colorSet.HexColor(EAffix.Fire)}>{affinityDef(EElement.Fire)}</color>  <sprite name=Water><color={colorSet.HexColor(EAffix.Water)}>{affinityDef(EElement.Water)}</color>  <sprite name=Nature><color={colorSet.HexColor(EAffix.Nature)}>{affinityDef(EElement.Nature)}</color> (%) \n";
-
+            str += BattleStats.Range.ToString(u)+ "\n";
+            str += $"<sprite name=Speed> <color={colorSet.HexColor(EAffix.Speed)}>{BattleStats.Speed} </color> \n";
             return str;
         }
-
-        private string affinityDef(EElement ele)
-        {
-            if (BattleStats.GetDamageTaken(ele) == 100) return "--";
-            string str = "";
-            if (BattleStats.GetDamageTaken(ele) > 100)
-                str += "+";
-            str += (int) BattleStats.GetDamageTaken(ele) - 100;
-            str += "%";
-            return str;
-        }
-
+        
         public override string GetInfoDown()
         {
             return "";
