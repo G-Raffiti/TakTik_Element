7c1530e0fc0b6787652f6ce39ee18348fba5590f
BBprog <brice@zaclys.net>
2021-11-24 23:23:14 +0100

    base mono deck

---
+++ Assets/Scripts/Skills/AllDecksMono.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using _EventSystem.CustomEvents;
+using Grid;
+using Units;
+using UnityEngine;
+using UnityEngine.UI;
+using Void = _EventSystem.CustomEvents.Void;
+
+namespace Skills
+{
+    public class AllDecksMono : MonoBehaviour
+    {
+        public DeckMono Deck;
+        private static GameObject instance;
+        
+        private bool used = false;
+
+        [Header("Event Sender")] 
+        [SerializeField] private VoidEvent onActionDone;
+        
+        [Header("Event Listener")] 
+        [SerializeField] private UnitEvent onUnitStartTurn;
+        [SerializeField] private VoidEvent onShuffleDecks;
+        [SerializeField] private VoidEvent onBattleStart;
+
+        private void Start() 
+        {
+            DontDestroyOnLoad(gameObject.transform);
+            if (instance == null)
+                instance = gameObject;
+            else
+                Destroy(gameObject);
+
+            Deck = transform.GetComponentInChildren<DeckMono>();
+            
+            onUnitStartTurn.EventListeners += OnEventRaised;
+            onShuffleDecks.EventListeners += Shuffle;
+            onBattleStart.EventListeners += FirstShuffle;
+        }
+
+        private void OnDestroy()
+        {
+            onUnitStartTurn.EventListeners -= OnEventRaised;
+            onShuffleDecks.EventListeners -= Shuffle;
+            onBattleStart.EventListeners -= FirstShuffle;
+        }
+
+        private void FirstShuffle(Void _obj)
+        {
+            Deck.ShuffleDeck();
+            Deck.Draw();
+        }
+
+        private void Shuffle(Void _obj)
+        {
+            Shuffle();
+        }
+
+        public void Shuffle()
+        {
+            if (BattleStateManager.instance.PlayingUnit.BattleStats.AP < 1 || used) return;
+            BattleStateManager.instance.PlayingUnit.BattleStats.AP--;
+            used = true;
+            
+            Deck.ShuffleDeck();
+            onActionDone.Raise();
+        }
+
+        public void OnEventRaised(Unit item)
+        {
+            GameObject.Find("UI_BattleScene/Right/ShuffleBtn").GetComponent<Button>().onClick.RemoveAllListeners();
+            GameObject.Find("UI_BattleScene/Right/ShuffleBtn").GetComponent<Button>().onClick.AddListener(Shuffle);
+            used = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UserInterface/DeckMonoUI.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using _EventSystem.CustomEvents;
+using Grid;
+using Skills;
+using Units;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Class that Update the Skills UI on Battle Scene
+    /// </summary>
+    public class DeckMonoUI : MonoBehaviour
+    {
+        private AllDecks allDecks;
+        private Dictionary<SkillInfo, int> skills;
+        private Unit unit;
+
+        [SerializeField] private GameObject skillBtn;
+
+        [Header("Event Listener")]
+        [SerializeField] private UnitEvent onUnitStartTurn;
+        [SerializeField] private VoidEvent onSkillUsed;
+        [SerializeField] private VoidEvent onActionDone;
+
+        private IEnumerator Start()
+        {
+            allDecks = GameObject.Find("Decks").GetComponent<AllDecks>();
+            skills = new Dictionary<SkillInfo, int>();
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                skills.Add(transform.GetChild(i).gameObject.GetComponent<SkillInfo>(), transform.childCount - i - 1);
+            }
+            
+            int number = transform.GetSiblingIndex();
+            
+            yield return new WaitForSeconds(0.1f);
+            
+            foreach (SkillInfo _skillsKey in skills.Keys)
+            {
+                _skillsKey.Deck = allDecks.Decks[number];
+            }
+        }
+
+        private void OnEnable()
+        {
+            onSkillUsed.EventListeners += onSkillUsedRaised;
+            onUnitStartTurn.EventListeners += onUnitStartTurnRaised;
+            onActionDone.EventListeners += onSkillUsedRaised;
+        }
+
+        private void OnDisable()
+        {
+            onSkillUsed.EventListeners -= onSkillUsedRaised;
+            onUnitStartTurn.EventListeners -= onUnitStartTurnRaised;
+            onActionDone.EventListeners -= onSkillUsedRaised;
+        }
+
+        public void onUnitStartTurnRaised(Unit item)
+        {
+            if (item.playerNumber != 0)
+            {
+                unit = BattleStateManager.instance.Units.Find(Unit => Unit.playerNumber == 0);
+            }
+            else unit = item;
+            
+            UpdateDisplay();
+        }
+
+        public void onSkillUsedRaised<T>(T param)
+        {
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            foreach (KeyValuePair<SkillInfo,int> _skill in skills)
+            {
+                if (_skill.Key.Deck.Skills.Count - 1 < _skill.Value)
+                {
+                    foreach (Transform _child in _skill.Key.gameObject.transform)
+                    {
+                        _child.gameObject.SetActive(false);
+                    }
+                }
+                else
+                {
+                    foreach (Transform _child in _skill.Key.gameObject.transform)
+                    {
+                        _child.gameObject.SetActive(true);
+                    }
+                    _skill.Key.UpdateSkill(_skill.Value, unit);
+                    _skill.Key.DisplayIcon();
+                }
+            }
+            /*
+            if (BattleStateManager.instance.PlayingUnit.BattleStats.AP < 1 || FirstSkill.Cost > FirstSkill.Unit.BattleStats.AP)
+            {
+                frameOfFirstSkill.color = Color.grey;
+            }
+            else frameOfFirstSkill.color = Color.white;
+            */
+        }
+    }
+}
