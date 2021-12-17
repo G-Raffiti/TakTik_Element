f3a6ea0e7184bc44dcb75549e962394db80b570b
BBprog <brice@zaclys.net>
2021-12-17 03:25:32 +0100

    button end turn

---
+++ Assets/Scripts/UserInterface/BattleScene/BtnEndTurn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using _EventSystem.CustomEvents;
+using UnityEngine;
+
+namespace UserInterface.BattleScene
+{
+    public class BtnEndTurn : MonoBehaviour
+    {
+        [Header("Event Listener")]
+        [SerializeField] private BoolEvent onBattleEnd;
+        
+        [SerializeField] private BattleInventory_UI inventory;
+
+        private void OnEnable()
+        {
+            onBattleEnd.EventListeners += SetInactive;
+        }
+        
+        private void OnDisable()
+        {
+            onBattleEnd.EventListeners -= SetInactive;
+        }
+
+        public void SetInactive(bool winCondition)
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnMouseDown()
+        {
+            if (!inventory.isActiveAndEnabled) return;
+        }
+    }
+}
